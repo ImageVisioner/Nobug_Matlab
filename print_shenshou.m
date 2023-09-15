171d6cd948537cc3a698c2c84a2d788311f1d6f9
ImageVisioner <ImageVisioner@outlook.com>
2023-09-15 19:12:44 +0800

    Create print_shenshou.m

---
+++ print_shenshou.m
@@ -0,0 +1,27 @@
+function print_shenshou()
+    fprintf('\n');
+    fprintf('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n');
+    fprintf('　　　　　　　 ┏┓　 ┏┓+ +           \n');
+    fprintf('　　　　　　　┏┛┻━━━┛┻┓ + +        \n');
+    fprintf('　　　　　　　┃　　　　　　┃ 　          \n');
+    fprintf('　　　　　　　┃　　　━　　 ┃ ++ + +       \n');
+    fprintf('　　　　　　 ████━████  ┃+              \n');
+    fprintf('　　　　　　　┃　　　　　　　┃ +         \n');
+    fprintf('　　　　　　　┃　　　┻　　　┃            \n');
+    fprintf('　　　　　　　┃　　　　　　┃ + +         \n');
+    fprintf('　　　　　　　┗━┓　　　┏━┛              \n');
+    fprintf('　　　　　　　　 ┃　　　┃　　　　　　　　　　　\n');
+    fprintf('　　　　　　　　 ┃　　　┃ + + + +           \n');
+    fprintf('　　　　　　　　 ┃　　　┃　　　　Code is far away from bug with the animal protecting　　　          \n');
+    fprintf('　　　　　　　　 ┃　　　┃ + 　　　　神兽保佑,代码无bug　　\n');
+    fprintf('　　　　　　　　 ┃　　　┃    \n');
+    fprintf('　　　　　　　　 ┃　　　┃　　+　　　　　　　　　\n');
+    fprintf('　　　　　　　　 ┃　 　 ┗━━━┓ + +              \n');
+    fprintf('　　　　　　　　 ┃ 　　　　   ┣┓           \n');
+    fprintf('　　　　　　　　 ┃ 　　　　　 ┏┛           \n');
+    fprintf('　　　　　　　　 ┗┓┓┏━┳┓┏┛ + + + +      \n');
+    fprintf('　　　　　　　   ┃┫┫ ┃┫┫  \n');
+    fprintf('　　　　　　　   ┗┻┛ ┗┻┛+ + + +           \n');
+    fprintf('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n');
+    fprintf('\n');
+end
